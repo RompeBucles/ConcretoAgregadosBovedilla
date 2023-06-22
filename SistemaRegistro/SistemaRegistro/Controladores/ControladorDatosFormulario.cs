05ad452d1c3e0abb4d34d433aa356fd50ee03ea8
Omar <70543870+RompeBucles@users.noreply.github.com>
2023-06-21 21:47:15 -0600

    modelo
    ingresoDatos

---
+++ SistemaRegistro/SistemaRegistro/Controladores/ControladorDatosFormulario.cs
@@ -0,0 +1,37 @@
+﻿using SistemaRegistro.ConexionBD;
+using SistemaRegistro.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaRegistro.Controladores
+{
+    public class ControladorDatosFormulario
+    {
+        private Conexion ConexionBD = new Conexion(); //Mando a llamar a la conexion a la base de datos.
+        private SqlDataReader LeerFilas; //Permite renderizar los datos de la BD.
+
+        public void InsertarUsuario(modeloUsuarios usuarios)
+        {
+
+            SqlCommand comando = new SqlCommand("insertarUsuario");
+            comando.Connection = ConexionBD.AbrirConexion();
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@Nombre", usuarios.nombre);
+            comando.Parameters.AddWithValue("@ApellidoP", usuarios.apellidoP);
+            comando.Parameters.AddWithValue("@ApellidoM", usuarios.apellidoM);
+            comando.Parameters.AddWithValue("@Perfil", usuarios.perfil);
+            comando.Parameters.AddWithValue("@CorreoElectronico", usuarios.correo);
+            comando.Parameters.AddWithValue("@Telefono", usuarios.telefono);
+            comando.Parameters.AddWithValue("@Usuario", usuarios.usuario);
+            comando.Parameters.AddWithValue("@Contraseña", usuarios.contrasena);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            ConexionBD.CerrarConexion();
+        }
+    }
+}
--- SistemaRegistro/SistemaRegistro/Modelo/ModeloIngresoDatos.cs
+++ SistemaRegistro/SistemaRegistro/Modelo/ModeloIngresoDatos.cs
@@ -10,11 +10,29 @@ namespace SistemaRegistro.Modelo
     {
         public int id { get; set; }
         public string producto { get; set; }
+        public string tecnologia { get; set; }
+        public string otro { get; set; }
         public string nombreProceso { get; set; }
-        public string tipoProcesos { get; set;}
+        public string tipoProcesos { get; set; }
         public string autor { get; set; }
         public string correo { get; set; }
         public string nombreFlujoR { get; set; }
+        public string unidad { get; set; }
+        public decimal valorR { get; set; }
+        public string objetivo {get; set;}
+        public string limitesSistema { get; set; }
+        public Image Imagen { get; set; }    
+        public string tipoTecnologia { get; set; }
+        public string condicionesOperacion { get; set; }
+        public string fechaReferencia { get; set; }
+        public string datosValidos { get; set; }
+        public string descripcion { get; set; }
+        public int id_estado {get; set;  }
+        public int id_area {get; set;  }
+        public float latitud { get; set;}
+        public float longitud { get; set; }
+
+
        
     }
 }
